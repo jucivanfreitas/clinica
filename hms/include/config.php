ff42a6777f7a0db7a2a5dae8784f675f70e1c6a6
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-09-01 14:59:23 -0300

    Descrição das alterações

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','u642157280_clinicas');
-define('DB_PASS' ,'a$Al^S:U0');
+define('DB_PASS' ,'!7xamHYR');
 define('DB_NAME', 'u642157280_clinicas');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
