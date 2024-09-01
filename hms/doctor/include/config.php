833fdfa4e51d7d168bace39ca55d939ac5ff236b
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-09-01 14:57:39 -0300

    Descrição das alterações

--- hms/doctor/include/config.php
+++ hms/doctor/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
+define('DB_USER','u642157280_clinicas');
+define('DB_PASS' ,'a$Al^S:U0');
+define('DB_NAME', 'u642157280_clinicas');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
