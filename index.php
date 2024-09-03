9154eb7e948e8777ccf5727216f72592d7656dc4
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-09-02 21:33:12 -0300

    traduzindo

--- index.php
+++ index.php
@@ -48,6 +48,11 @@ if (isset($_POST['submit'])) {
 
 
 <head>
+<?php
+// Carrega as traduções
+$translations = include 'translations.php';
+?>
+
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
