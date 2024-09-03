7cf431eb29c236ae85581d40a99f40fe821ed97a
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-09-02 21:34:25 -0300

    traduzindo

--- index.php
+++ index.php
@@ -48,11 +48,6 @@ if (isset($_POST['submit'])) {
 
 
 <head>
-<?php
-// Carrega as traduções
-$translations = include 'translations.php';
-?>
-
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -76,7 +71,7 @@ $translations = include 'translations.php';
         <div id="nav-head" class="header-nav">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;"><?= $translations['sigla'] ?>
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;">HMS
                        <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
                     </div>
                     <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
