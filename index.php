9cf514dd50d78a07dfea9c52a5a52bc86d20af28
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-09-02 21:31:54 -0300

    traduzindo

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ if (isset($_POST['submit'])) {
         <div id="nav-head" class="header-nav">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;">HMS
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:42px; margin-top: 1% !important;"><?= $translations['sigla'] ?>
                        <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
                     </div>
                     <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
--- translations.php
+++ translations.php
@@ -2,6 +2,7 @@
 // translations.php
 return [
     'Nome_app' => 'Sistema de Gestão Hospitalar',
+    'sigla'=> 'SGH'
     'home' => 'Início',
     'services' => 'Serviços',
     'about_us' => 'Sobre Nós',
